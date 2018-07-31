f32ff29e61347b5197d19b9af020770788ab335e
ckaatz <39869540+ckaatz@users.noreply.github.com>
2018-07-30 20:08:39 -0500

    Add project files.

---
+++ C-Step96/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace C_Step96
+{
+    class Program
+    {
+        static void Main()
+        {
+            string Concat1 = "This is ";
+            string Concat2 = "a ";
+            string Concat3 = "simple sentence.";
+            Console.WriteLine(Concat1 + Concat2 + Concat3);
+
+            Console.WriteLine("Write any word:");
+            string Upper = Console.ReadLine();
+            Console.WriteLine(Upper.ToUpper());
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("One fish, ");
+            sb.Append("two fish. ");
+            sb.Append("Red fish, ");
+            sb.Append("blue fish. ");
+            Console.WriteLine(sb);
+
+            Console.ReadLine();
+        }
+    }
+}
